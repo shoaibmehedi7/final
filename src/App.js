7f619665181ccb1accaf315dba7ea411d464d27d
darkshadow <shoaib.mehedi@gmail.com>
2019-01-13 19:34:56 +0600

    about to finish

--- src/App.js
+++ src/App.js
@@ -5,28 +5,32 @@ import Download from "./Download/Dow";
 import Works from "./Works/works";
 import Card from "./Card/Card";
 import GettingStarted from "./GettingStarted/GettingStarted";
-import Footer from './Footer/Footer'
-import CardDot from './CardDot/CardDot'
+import Footer from "./Footer/Footer";
+import CardDot from "./CardDot/CardDot";
+import Form from "./Form/Form";
 
 class App extends Component {
   render() {
-    return (
-      <div >
+    return <div>
         <Nav />
         <Download />
-        <h1><br/></h1>
-        <CardDot/>
-        <h1><br/></h1>
+        <h1>
+          <br />
+        </h1>
+        <CardDot />
+        <h1>
+          <br />
+        </h1>
         <Works />
         <br />
         <Card />
         <br />
         <GettingStarted />
         <br />
+        <Form/>
         <br />
-        <Footer/>
-      </div>
-    );
+        <Footer />
+      </div>;
   }
 }
 
---
+++ src/Form/Form.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import './Form.css'
+export class Form extends Component {
+  render() {
+    return (
+      <div>
+        <div className="container text-center">
+          <h2>Why are you waiting?</h2>
+          <h4>Fill the form to hear from us.</h4>
+          <form class="form-control">
+            <input class="form-control-lg" type="text" name="fname" placeholder="Name" />
+            <br />
+            <input class="form-control-lg" type="text" name="fname" placeholder="Mobile No" /><br />
+            <input class="form-control-lg" type="text" name="fname" placeholder="Address" />
+            <br />
+            <input class="form-control-lg" type="submit" value="Request a call" />
+          </form>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Form;
