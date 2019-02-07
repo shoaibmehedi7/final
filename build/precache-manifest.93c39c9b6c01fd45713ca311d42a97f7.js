5a7aaf5262cbeb8ca31fc8f6958036c48f820e1d
darkshadow <shoaib.mehedi@gmail.com>
2019-02-07 10:51:59 +0600

    with git module

---
+++ build/precache-manifest.93c39c9b6c01fd45713ca311d42a97f7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c4959bc6c2c461feb7ffa0d8a8e37a3",
+    "url": "/static/media/demo.3c4959bc.png"
+  },
+  {
+    "revision": "63144e22ceb5f2caf63f",
+    "url": "/static/css/main.27965938.chunk.css"
+  },
+  {
+    "revision": "84d3190991030dc58ce2e954dbd03abb",
+    "url": "/static/media/four.84d31909.jpg"
+  },
+  {
+    "revision": "eff1f06cac348df8eb6a",
+    "url": "/static/js/1.eff1f06c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "63144e22ceb5f2caf63f",
+    "url": "/static/js/main.63144e22.chunk.js"
+  },
+  {
+    "revision": "8216401f861a1607015cbf384ee30b5f",
+    "url": "/static/media/one.8216401f.jpg"
+  },
+  {
+    "revision": "e21d1e0949fe8d492a78417bfbd65a72",
+    "url": "/static/media/two.e21d1e09.jpg"
+  },
+  {
+    "revision": "4551832282db6fc14ab1397ba8d4bc72",
+    "url": "/static/media/three.45518322.jpg"
+  },
+  {
+    "revision": "eff1f06cac348df8eb6a",
+    "url": "/static/css/1.a9d319e8.chunk.css"
+  },
+  {
+    "revision": "44cbb73b06a1889e877fc904d0ece853",
+    "url": "/index.html"
+  }
+];
